bdbf82c71747719a459a02ae66bd7ffd92f2d953
Jose Espinoza <137861752+JoseEspinoza12@users.noreply.github.com>
2024-12-05 01:00:40 -0600

    cambios en la configuracion de la web

---
+++ src/main/java/com/inventarioDulceria/CRUD/config/WebConfig.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.inventarioDulceria.CRUD.config;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // Permitir todas las rutas
+                .allowedOrigins("*") // Permitir todos los orígenes
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Métodos HTTP permitidos
+                .allowedHeaders("*") // Permitir todos los encabezados
+                .allowCredentials(false); // No permitir credenciales (por seguridad)
+    }
+}
